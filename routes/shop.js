e96a43383341a014c25aef0d91e14dfa42599f28
Roman Kushnir <rkushni@exadel.com>
2024-06-24 19:40:35 +0300

    use pug as template engine

--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const express = require('express');
 const rootDir = require('../utils/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.render('shop', {prods: adminData.products, pageTitle: 'Shop', path: '/'});
 });
 
 module.exports = router;
